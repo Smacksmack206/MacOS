a9390355b42da20b055de9f9320f0fcc2e6e2d8f
Cedric Vallieu <cvallieu94@gmail.com>
2021-10-08 10:20:12 -0700

    script to fix issues with Clipboard app Maccy not displaying saved clipboards, made to be executable

---
+++ resetMaccy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+defaults write org.p0deje.Maccy migrations -dict "2020-02-22-history-item-add-copied-at" 1 \
+                                                 "2020-02-22-history-item-add-number-of-copies" 1 \
+                                                 "2020-04-18-switch-storage-to-core-data" 1 \
+                                                 "2020-04-25-allow-custom-ignored-types" 1 \
+                                                 "2020-06-19-use-keyboardshortcuts" 1 \
+                                                 "2020-09-01-ignore-keeweb" 1 \
+                                                 "2021-02-20-allow-to-customize-supported-types" 1
